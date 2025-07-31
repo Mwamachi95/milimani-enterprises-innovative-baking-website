32b89e70af4009b6f79a3693f988e58509420849
Lenny Toroitich <lktoroitich@gmail.com>
2025-07-31 18:25:18 +0300

    Improved the overall responsivity of the components.

---
+++ milimani_enterprises/src/components/AboutUsHero/AboutUsHero.jsx
@@ -0,0 +1,27 @@
+function AboutUsHero(){
+    return(
+        <>
+        <div className="md:pl-15 md:pr-0 md:pt-15 md:pb-15 pl-8 pr-8 pt-40 pb-40 ">
+            <div className="sm:w-screen flex items-center justify-between md:gap-5 xl:gap-40 xl:justify-center overflow-hidden">
+
+                {/* Text Section */}
+                <div className="flex flex-col gap-4 items-start text-[#143e45] ">
+                    <h1 className="text-6xl md:text-7xl font-extrabold tracking-wider max-w-160">Where We Bake Innovatively</h1>
+                    <p className="text-sm md:text1 max-w-155">Milimani Enterprises supplies quality baking ingredients and equipment across Rwanda. We blend innovation with a personal touch to help bakers succeed.</p>
+                    {/* Learn more button */}
+                    <div className="p-3 rounded-sm flex gap-2 cursor-pointer bg-[#67a650] hover:bg-[#5b9b4e] transition duration-300">
+                        <p className="text-white">Learn more</p>
+                        <img src="arrowRight.svg" alt="Arrow right"></img>
+                    </div>
+                </div>
+
+                {/* Image Section */}
+                <img src="baker.svg" alt="Baker illustration" className="hidden md:block"/>
+                
+            </div>          
+        </div>
+        </>
+    );
+}
+
+export default AboutUsHero;
--- milimani_enterprises/src/pages/AboutUs.jsx
+++ milimani_enterprises/src/pages/AboutUs.jsx
@@ -1,9 +1,12 @@
 import Footer from "../components/Footer/footer";
 import OurValues from "../components/OurValues/ourValues";
 import OurTeam from "../components/OurTeam/OurTeam";
+import AboutUsHero from "../components/AboutUsHero/AboutUsHero";
 function aboutUs() {
   return (
     <>
+      {/* About Us Hero Section */}
+      <AboutUsHero />
       
       {/* Our Values Section */}
       <OurValues />
